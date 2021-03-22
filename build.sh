2d49cdff82c1804dd0398f6046d1098e2f4b76de
WhysDev <78435081+WhysDev@users.noreply.github.com>
2021-03-22 19:45:14 +0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+export KERNELNAME=WhysTomcats-V1-eas
+
+export LOCALVERSION=v1.0 stable
+
+export KBUILD_BUILD_USER=WhysDev
+
+export KBUILD_BUILD_HOST=droneci
+
+export TOOLCHAIN=clang
+
+export DEVICES=whyred
+
+export CI_ID=-1420291641
+
+export GROUP_ID=-460343147
+
+source helper
+
+gen_toolchain
+
+send_msg "⏳ Start building ${KERNELNAME} ${LOCALVERSION} | DEVICES: whyred - tulip - lavender - wayne - jasmine"
+
+send_pesan "⏳ Start building ${KERNELNAME} ${LOCALVERSION} | DEVICES: whyred - tulip - lavender - wayne - jasmine"
+
+START=$(date +"%s")
+
+for i in ${DEVICES//,/ }
+do
+	build ${i} -oldcam
+
+	build ${i} -newcam
+done
+
+send_msg "⏳ Start building Overclock version | DEVICES: whyred - tulip"
+
+send_pesan "⏳ Start building Overclock version | DEVICES: whyred - tulip"
+
+git apply oc.patch
+
+for i in ${DEVICES//,/ }
+do
+	if [ $i == "whyred" ] || [ $i == "tulip" ]
+	then
+		build ${i} -oldcam -overclock
+
+		build ${i} -newcam -overclock
+	fi
+done
+
+END=$(date +"%s")
+
+DIFF=$(( END - START ))
+
+send_msg "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s,get TEST build in @NightmareCI | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
+
+send_pesan "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
+© 2021 
