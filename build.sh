e73a312c1e6e35fd2ef4da0266de4dd48de3f0b9
WhysDev <78435081+WhysDev@users.noreply.github.com>
2021-03-22 21:22:12 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,9 +20,9 @@ source helper
 
 gen_toolchain
 
-send_msg "⏳ Start building ${KERNELNAME} ${LOCALVERSION} | DEVICES: whyred - tulip - lavender - wayne - jasmine"
+send_msg "⏳ Start building ${KERNELNAME} ${LOCALVERSION} | DEVICES: whyred"
 
-send_pesan "⏳ Start building ${KERNELNAME} ${LOCALVERSION} | DEVICES: whyred - tulip - lavender - wayne - jasmine"
+send_pesan "⏳ Start building ${KERNELNAME} ${LOCALVERSION} | DEVICES: whyred"
 
 START=$(date +"%s")
 
@@ -33,15 +33,15 @@ do
 	build ${i} -newcam
 done
 
-send_msg "⏳ Start building Overclock version | DEVICES: whyred - tulip"
+send_msg "⏳ Start building Overclock version | DEVICES: whyred"
 
-send_pesan "⏳ Start building Overclock version | DEVICES: whyred - tulip"
+send_pesan "⏳ Start building Overclock version | DEVICES: whyred"
 
 git apply oc.patch
 
 for i in ${DEVICES//,/ }
 do
-	if [ $i == "whyred" ] || [ $i == "tulip" ]
+	if [ $i == "whyred" ]
 	then
 		build ${i} -oldcam -overclock
 
@@ -53,7 +53,7 @@ END=$(date +"%s")
 
 DIFF=$(( END - START ))
 
-send_msg "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s,get TEST build in @NightmareCI | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
+send_msg "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s,get TEST build in @WhysCL | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
 
 send_pesan "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
 © 2021 
